ef4d83427a15621714bb44e6806dfb76dfa38213
priyanka408 <112479702+priyanka408@users.noreply.github.com>
2022-09-16 15:53:20 +0530

    Add files via upload

--- ExampleInstrumentedTest.java
+++ ExampleInstrumentedTest.java
@@ -1,9 +1,14 @@
-package com.quintus.labs.grocerystore;
+package com.example.food_app;
 
-import androidx.test.runner.AndroidJUnit4;
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,6 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.food_app", appContext.getPackageName());
     }
 }
